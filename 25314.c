94a8aa4a410537ade0aaee36a3b323b4e113f1c6
rhdwn22c <rhdwn22c@gmail.com>
2022-07-19 17:14:09 +0900

    baekjoon 25314

---
+++ 25314.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    if(n % 4 != 0) {
+        return 0;
+    }
+
+    for(int i = 0; i < n / 4; i++) {
+        printf("long ");
+    }
+    printf("int");
+}
