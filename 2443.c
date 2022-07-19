96885c4f66f9373ce6078d4179f859768c2e9dd2
rhdwn22c <rhdwn22c@gmail.com>
2022-07-19 17:41:50 +0900

    baekjoon 2443

---
+++ 2443.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    scanf("%d", &a);
+
+    for (int i = a; i >= 1; i--) {
+        for (int k = a - 1; k >= i; k--) {
+            printf(" ");
+        }
+        for (int j = 2 * i - 1; j >= 1; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
