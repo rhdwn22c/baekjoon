8c4fb26a5d9ea199a8ee24a36a05d10e08baf6e0
rhdwn22c <rhdwn22c@gmail.com>
2022-08-14 20:40:18 +0900

    baekjoon 1978

---
+++ 1978.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int n, temp, cnt = 0;
+
+    scanf("%d", &a);
+
+    for(int i = 0; i < a; i++) {
+        scanf("%d", &n);
+        temp = 0;
+
+        if(n == 1) {
+            continue;
+        }
+        for(int j = 2; j < n; j++) {
+            if(n % j == 0) {
+                temp = 1;
+            }
+            if(temp == 0) {
+                cnt++;
+            }
+        }
+        printf("%d", cnt);
+    }
+}
